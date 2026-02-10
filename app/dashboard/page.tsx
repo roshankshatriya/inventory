70ce3229466288c53dc997f4dc43db9e6d3a6acf
roshankshatriya <roshankshatriya75@gmail.com>
2026-02-10 17:59:15 +0530

    inventory base layout is done

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,29 @@
+
+import Sidebar from "@/components/sidebar";
+import { TrendingUp } from "lucide-react";
+
+export default async function DashboardPage() {
+
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Sidebar currentPath="/dashboard" />
+      <main className="ml-64 p-8">
+        {/* Header */}
+        <div className="mb-8">
+          <div className="flex items-center justify-between">
+            <div>
+              <h1 className="text-2xl font-semibold text-gray-900">
+                Dashboard
+              </h1>
+              <p className="text-sm text-gray-500">
+                Welcome back! Here is an overview of your inventory.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        
+      </main>
+    </div>
+  );
+}
